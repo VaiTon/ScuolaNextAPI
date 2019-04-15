28cb28a03a0094cfac4348958c541531756b313f
VaiTon <eyadlorenzo@gmail.com>
2019-04-15 12:36:22 +0200

    Fixed custom errors (See https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work)

--- src/errors.ts
+++ src/errors.ts
@@ -1,10 +1,13 @@
-export class TimeoutError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
 export class AuthError extends Error {
   constructor(message: string) {
     super(message);
+    Object.setPrototypeOf(this, AuthError.prototype);
+  }
+}
+export class TimeoutError extends Error {
+  constructor(m: string) {
+    super(m);
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, TimeoutError.prototype);
   }
 }
