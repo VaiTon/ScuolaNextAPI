1b551b163c060c8e0bf3b7dadf046f6d51644e04
vaiton <eyadlorenzo@gmail.com>
2019-06-13 00:24:00 +0200

    Adapted to the new Argo API changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,14 @@
 export const ARGO_API_URL = 'https://www.portaleargo.it/famiglia/api/rest/';
 export const ARGO_API_KEY = 'ax6542sdru3217t4eesd9';
 export const ARGO_API_VERSION = '2.0.12';
+export const ARGO_PRODUCER = 'ARGO Software s.r.l. - Ragusa';
+export const ARGO_APP_KEY = 'APF';
 
 export const ARGO_DEF_HEADERS = {
   'x-key-app': ARGO_API_KEY,
-  'x-version': ARGO_API_VERSION
+  'x-version': ARGO_API_VERSION,
+  'x-app-code': ARGO_APP_KEY,
+  'x-produttore-software': ARGO_PRODUCER
 };
 
 export const NO_VOTE = [
